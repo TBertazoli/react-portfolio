0baa8df19f822c5420bda5ef424e315115921402
TB <tatianabertazoli@gmail.com>
2023-01-06 23:22:08 -0600

    created resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Footer from '../Footer'
+
+function Resume() {
+    return (
+        <section>
+            <div className="cover">
+                <h2 className="subtitle">React Professional Portfolio</h2>
+            </div>
+            <div id="about" className="about_me">
+                <div className="title">
+                    <h2>About Me</h2>
+                </div>
+                <div className="description">                    
+                    <p></p>
+                </div>
+            </div>
+            <Footer></Footer>
+        </section>
+
+    )
+}
+
+export default Resume;
